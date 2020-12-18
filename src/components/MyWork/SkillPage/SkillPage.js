5c2038db59bd047d02f1cbc8779ac92799edd985
RanningMan <xiaranforusa@gmail.com>
2020-12-18 14:56:07 -0800

    Re-organize skills

--- src/components/MyWork/SkillPage/SkillPage.js
+++ src/components/MyWork/SkillPage/SkillPage.js
@@ -8,36 +8,40 @@ import OtherPage from '../../../hoc/OtherPage';
 import Skill from './Skill';
 import classes from './SkillPage.module.css'
 
-const skillList = {
+const technology = {
     'HTML(5)': 5,
     'CSS(3)': 5,
     'JavaScript': 5,
     'TypeScript': 5,
     'React (Redux)': 5,
-    'Algorithms': 5,
     'node.js': 5,
     'AngularJS': 4,
     'MondoDB': 4,
     'C++': 4,
     'Tableau': 4,
-    'Data Visualization': 4,
-    'Refactoring': 4,
     'Git': 4,
-    'Agile': 4,
-    'Serverless': 4,
     'GraphQL': 3,
-    'Accessibility': 3,
     'C#': 3,
-    'Machine Learning': 3,
     'Java': 2,
     'SQL': 2,
 }
 
-const skill = () => {
+const domains = {
+    'Graph Algorithms': 5,
+    'Web Performance': 5,
+    'Data Visualization': 4,
+    'Refactoring': 4,
+    'Agile': 4,
+    'Serverless': 4,
+    'Accessibility': 3,
+    'Machine Learning': 3,
+}
+
+const buildSkillTable = (skillType, skillList) => {
     let left = [], right = [];
-    if(window.innerWidth > 767) {
+    if (window.innerWidth > 767) {
         Object.keys(skillList).forEach((key, index) => {
-            if(index % 2 === 0) {
+            if (index % 2 === 0) {
                 left.push({
                     name: key,
                     star: skillList[key]
@@ -60,24 +64,35 @@ const skill = () => {
         });
     }
 
+    return <>
+        <Row className={classes.SkillType}><Col md={12}><h3>{skillType}</h3></Col></Row>
+        <Row className={classes.SkillPage}>
+            <Col md={6}>
+                <ListGroup>
+                    {
+                        left.map(skill => <Skill name={skill.name} starLevel={skill.star} key={skill.name}></Skill>)
+                    }
+                </ListGroup>
+            </Col>
+            <Col md={6}>
+                <ListGroup>
+                    {
+                        right.map(skill => <Skill name={skill.name} starLevel={skill.star} key={skill.name}></Skill>)
+                    }
+                </ListGroup>
+            </Col>
+        </Row>
+    </>;
+}
+
+const skill = () => {
+    const Tech = buildSkillTable('Technology', technology);
+    const Domain = buildSkillTable('Domains', domains);
+
     return (
         <OtherPage title="Skills" qoute="Success is that old ABC - ability, breaks and courage. -- Charles Luckman" backgroundStyle="resume-dark">
-            <Row className={classes.SkillPage}>
-                <Col md={6}>
-                    <ListGroup>
-                        {
-                            left.map(skill => <Skill name={skill.name} starLevel={skill.star} key={skill.name}></Skill>)
-                        }
-                    </ListGroup>
-                </Col>
-                <Col md={6}>
-                    <ListGroup>
-                        {
-                            right.map(skill => <Skill name={skill.name} starLevel={skill.star} key={skill.name}></Skill>)
-                        }
-                    </ListGroup>
-                </Col>
-            </Row>
+            {Tech}
+            {Domain}
         </OtherPage>
     );
 }
