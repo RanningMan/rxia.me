099e4cf05394aaa07dd8568dddaa667cf9ffe14b
RanningMan <xiaranforusa@gmail.com>
2021-11-07 10:41:49 -0800

    Add blog but disable fornow

--- src/components/MyWork/FirstPage/FirstPage.js
+++ src/components/MyWork/FirstPage/FirstPage.js
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 
 import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
@@ -9,7 +9,19 @@ import ThemeContext from '../Theme/ThemeContext';
 import classes from './FirstPage.module.css';
 import Background from '../../../asset/Flash-left.svg';
 import MobileBackground from '../../../asset/Mobile-Background.png';
+import Blog from '../../../asset/blogger.png';
+import B from '../../../asset/bloggerB.png';
 
+const BlogSwitcher = () => {
+    const [hover, setHover] = useState(false);
+
+    return <a className={classes.Blog} 
+              href='https://rxia.blog/'
+              onMouseOver={() => setHover(true)} 
+              onMouseLeave={() => setHover(false)}>
+                  <img src={hover ? Blog : B} alt="check out my blogs!" title="check out my blogs!"/>
+            </a>;
+}
 
 const FirstPage = ({isLargeScreen}) => {
 
@@ -20,12 +32,15 @@ const FirstPage = ({isLargeScreen}) => {
         backgroundColor: themeStyle.dominant
     }
 
+    const isBlogReady = false;
+
     return (
         <>
             <img src={isLargeScreen ? Background : MobileBackground} alt='' className={classes.Image}
                  style={styles}/>
             <Row className={classes.FirstPage}>
                 {isLargeScreen && <ThemeSwitcher />}
+                {isBlogReady && isLargeScreen && <BlogSwitcher />}
                 <Col></Col>
                 <Col>
                     <NameTag></NameTag>
